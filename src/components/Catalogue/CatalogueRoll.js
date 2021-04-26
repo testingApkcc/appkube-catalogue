32abbda6de85a2c6c285edb5b543e3790db21c67
Jasmin Makasana <jasminmakasana1991@gmail.com>
2021-04-26 10:23:23 +0530

    Update: img path changed.

--- src/components/Catalogue/CatalogueRoll.js
+++ src/components/Catalogue/CatalogueRoll.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql, StaticQuery } from 'gatsby'
-import isSearch from '../img/search-icon.png'
+import isSearch from '../../img/search-icon.png'
 import AddLibraryPopup from './AddLibraryPopup';
 import AddCataloguePopup from './AddCataloguePopup';
 import { kebabCase } from 'lodash';
